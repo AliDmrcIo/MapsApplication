4da1e1d8dc54b0215696ef0a043aac240fb881a3
AliBaba61 <110434358+AliBaba61@users.noreply.github.com>
2023-05-17 16:06:59 +0300

    Update MapsActivity.java

--- app/src/main/java/com/ali/javamaps/view/MapsActivity.java
+++ app/src/main/java/com/ali/javamaps/view/MapsActivity.java
@@ -42,13 +42,7 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnMapLongClickListener {//bu implements den sonraki 2.ifadeyi ben yazdım ki uzun tıklama için gereklidir. Sonra bana bu inteface ile alakalı hata verecek ve bazı methodları uygulamamı isteyecek
 
-    //Burada maps kullanacaksak android studio bizden gidip Google Maps API key edinmemiz gerektiğini söylüyor ve biz de gidip AndroidManifst.xml dosyasında yazan adresten api keimizi aldık ve manifest xmlinde meta-data nın altında bulunan android:value="AIzaSyAooZkel6tKV54LLX1Fs0P9HaKCas8T8qo" /> yerine kopyaladık
-
-    //Sqlin bu insert intoları select*from ları falanları filanlarından kurtulmak ve daha kolay bu sql i kullanmak için room kütüphanesi yazlmış ve onu kullanacağız biz de. Burada kullanmak için öncelikle gradle.app'e gidip dependencies altında 5 satır kod kopyaladım. Oraya bakabilirsin. Onları Kullanabilmek için onları oraya kopyalamam gerekliydi ve RXjava denen bir şeyi de öğreneceğiz. Dependecy altında kopyalanacak o 5 satır kodu bulabilmek için google'a room database android yazıp androidin sitesine gidebiliriz. Orada yazıyor ne kopyalanacağı
-    //Bu room'un 3 tane ana temel yapısı var. Room Database(Normal database kısmı) Data Access Object(Veriye erişme objesi. Verileri alırken ya da silerken bu arayüzü kullancağız.) ve son olarak Entities(Bu da modelimiz, kolonlarda ne olacak değerleri ne olacak vs, daha önce yaptığımız create table if not exists falan işleri)
-    //Bu entites kısmını yapmak için aynı google'a room database android yazdığımızda çıkan sayfadan altta tanımlamamız gereken sınıfları vs tanımlamalıyız. Bunlar karışmasın diye gidip şimdi bir package açıcam
-
-    private GoogleMap mMap;
+        private GoogleMap mMap;
     private ActivityMapsBinding binding;
     LocationManager locationManager;
     LocationListener locationListener;
@@ -62,7 +56,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     double selectedLatitude;
     double selectedLongitude;
 
-    private CompositeDisposable compositeDisposable=new CompositeDisposable();//disposible yani tek kullanımlık. Bu yaptığımız Completablelar, flowablelar, singellar observeballar, bunların hepsi kullan at bir torbada tutulabiliyor. Yani bunların varlığı hafızada yer kaplamadan, 1 kere kullandıktan sonra oraya(en altta yazdım onDestroy methodunda) atılabiliyor
+    private CompositeDisposable compositeDisposable=new CompositeDisposable();
 
 
     @Override
@@ -80,9 +74,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         registerLauncher();
 
         db= Room.databaseBuilder(getApplicationContext(),PlaceDatabase.class,"Places")
-                //.allowMainThreadQueries() MainThread'de işlem yapılmasına izin ver dedik, artık çöken uygulamamız çökmeyecek
-                .build();//Database bu şekilde main class'ta initialize ediliyor. oluşturduğumuz database class'ını kullanmak ve oradan bir nesne oluşturmak için bunu yazdık. İnitialize ettik aslında yani, şunun gibi bişe. PlaceDatabase db=new PlaceDatabase(); gibi bir şey bu yaptığımız.
-        placeDao=db.placeDao(); //bu da daoya erişebileceğimiz bir initialize etme yöntemi. Dao da da böyle initialize edilir
+                .build();
+        placeDao=db.placeDao();
 
         selectedLatitude=0.0;
         selectedLongitude=0.0;
@@ -92,38 +85,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     @Override
-    public void onMapReady(GoogleMap googleMap){//onCreate yerine bu projede bir çok yerde onMapReady içerisine bir şeyler yazacağız.
+    public void onMapReady(GoogleMap googleMap){
         mMap = googleMap;
-        mMap.setOnMapLongClickListener(this);//en aşağıdaki onMapLongClick methodunun çalışabilmesi için yazılması gereken bir method
+        mMap.setOnMapLongClickListener(this);
 
         binding.saveButton.setEnabled(false);
         binding.deletButton.setEnabled(false);
 
-        Intent intent=getIntent();//buraya gelecek olan intent'in new veya old olmasına göre farklı reaksiyonlar alacağız
+        Intent intent=getIntent();
 
         String Intentinfo=intent.getStringExtra("info");
 
-        if(Intentinfo.equals("new")){//yeni bir şey konulmak isteniyor yani, eğer menüden 'Add Location' a basmışsa kullanıcı yeni bir lokasyon eklemek istiyor
+        if(Intentinfo.equals("new")){
 
-            binding.saveButton.setVisibility(View.VISIBLE);//save butonu gösterilsin
-            binding.deletButton.setVisibility(View.GONE);//delete butonunu yeni bir şey eklenmek istediği zaman tamamen yok et
+            binding.saveButton.setVisibility(View.VISIBLE);
+            binding.deletButton.setVisibility(View.GONE);
 
-            //kullanıcının nerede olduğunu almak için 2 tane şey kullanmalıyız. 1) LocationManager   2) LocationListener
-            locationManager=(LocationManager) this.getSystemService(Context.LOCATION_SERVICE);//android işletim sisteminden lokasyon servislerini alyor
+            
+            locationManager=(LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
             locationListener=new LocationListener() {
                 @Override
-                public void onLocationChanged(@NonNull Location location) {//konum değiştiğinde ne olacağını yazdığımız yer
-                    //bu move to camera kodlarından dolayı uygulama düzgün çalışmıyor. Uygulama açılıyor ve sonrasında haritayı kullanıcı hareket ettirse dahi bu kodlar yüzünden kamera hep bulunduğumuz konuma gidiyor. Yüzden şimdi buraya uygulama sadece ilk açıldığında kamerayı konumuma götür algoritması yazacağız.
-
+                public void onLocationChanged(@NonNull Location location) {
+                    
                     SharedPreferences sharedPreferences=MapsActivity.this.getSharedPreferences("com.ali.javamaps",MODE_PRIVATE);
-                    boolean info=sharedPreferences.getBoolean("info",false);//uygulama ilk açılığında info diye bi şey olmadığı için false döndürecek ama sonrasında info olacağından true olacak
-
+                    boolean info=sharedPreferences.getBoolean("info",false);
                     if(info==false){
-                        LatLng userLocation=new LatLng(location.getLatitude(),location.getLongitude());//Location veritipini Latlng'a çevirdik çünkü moveCamera fonksiyonu içerisinde bizden Latlng veritipini istyor ama bizim locationumuz location veritipinde
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,15));//kamerayı kullanıcının bulunduğu yere götür ve zoomla
+                        LatLng userLocation=new LatLng(location.getLatitude(),location.getLongitude());
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,15));
                         mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location!"));
 
-                        sharedPreferences.edit().putBoolean("info",true).apply();//şidmi sharedPreferences içerisindeki değeri false aptık ve bu kod bloğu tekrar çalıştığında burası true olacağından doalyı if bloğuna girmeyecek kod ve bize yaşattığı kameranın oynamaması sorununu bu algoritmayla çözmüş olduk
+                        sharedPreferences.edit().putBoolean("info",true).apply();
 
                     }
 
@@ -131,19 +122,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
 
                 @Override
-                public void onStatusChanged(String provider, int status, Bundle extras) {//bazen bunu yazmazsak sorun oluyor diye biz de yazdık. LocationListener içerisinde onSt yazarsak zaten direk burayı çıkartıyor. Ezber yani bura, takılcak bir şey yok.
+                public void onStatusChanged(String provider, int status, Bundle extras) {
                     LocationListener.super.onStatusChanged(provider, status, extras);
                 }
             };
 
-
-
-            //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener); kullanıcıdan lokasyon değişimlerini iste
-
-
             if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
                 //kullanıcıdan izin al
-                if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){//burası android tarafından kullanici iknasi icin kullanilan alan
+                if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
                     //kullanıcıdan izin al
                     Snackbar.make(binding.getRoot(),"Permission needed for maps",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
                         @Override
@@ -156,29 +142,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
                 }else{
-                    //kullanıcıyı ikna et, edilmezse tekrar izin iste
-                    permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);//iznimizi istedik
+                    permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
 
                 }
 
             }else{
-                //izin verilmiş zaten. kullanıcıdan verileri al
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);//konumu alma
+               
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
 
-                //uygulama kapatılıp açıldıktan sonra kamera direkt dünya ölçeğine çıkıyor ve çok uzakta kalıyor sonrasında neredeysek oraya odaklıyor ancak biz direkt son bilinen konum(last known location) 'da uygulamayı başlatmak istiyoruz. o yüzden şimdi bazı kodlar yazacağız
-                Location lastLocation= locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);//son bilinen konumu bu şekilde alabiliyoruz
+                Location lastLocation= locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-                if(lastLocation!=null){//eğer son lokasyon verisi boş değilse if'i çalıştır
+                if(lastLocation!=null){
                     LatLng lastLocationLatlng=new LatLng(lastLocation.getLatitude(),lastLocation.getLongitude());
                     mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastLocationLatlng,15));
                 }
 
-                mMap.setMyLocationEnabled(true);//konumumun doğruluğundan emin ol dedik ve o haritada ki mavi çember çıktı
+                mMap.setMyLocationEnabled(true);
 
             }
 
 
-        }else{  //kaydettiği lokasyona bakmak istiyor
+        }else{  
 
             mMap.clear();
             selectedPlace = (Place) intent.getSerializableExtra("place");
@@ -204,9 +188,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 if(result==true){
 
                     if(ContextCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
-                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);//konumu alma
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
 
-                        Location lastLocation= locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);//son bilinen konumu bu şekilde alabiliyoruz
+                        Location lastLocation= locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
                         if(lastLocation!=null){//eğer son lokasyon verisi boş değilse if'i çalıştır
                             LatLng lastLocationLatlng=new LatLng(lastLocation.getLatitude(),lastLocation.getLongitude());
@@ -230,44 +214,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    @Override//  mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location!"));
+    @Override
 
-    public void onMapLongClick(@NonNull LatLng latLng) {//uzun basma özelliği için ennn yukarıdaki satırda(MapsActivity de) implemente ettiğimiz GoogleMap.OnMapLongClickListener satırı hata verince methodları ugula dedik ve bura yazıldı. İçeride ki Latlang latlang da bize nereye tıklandığının enlem boylamını veriyor.
-        mMap.clear();//Her tıklanmadan sonra haritayı markerlardan temizliyor. Bu da her işaretlenen yerin haritada kalmamasını, her işaretlemeden sonra eskisinin silinmesini sağlıyor.
+    public void onMapLongClick(@NonNull LatLng latLng) {
+        mMap.clear();
         mMap.addMarker(new MarkerOptions().position(latLng).title("You choose here!"));
 
         selectedLatitude=latLng.latitude;
         selectedLongitude=latLng.longitude;
 
-        binding.saveButton.setEnabled(true);//kullanıcı haritada bir yer seçene kadar save butonu aktif olmayacak
+        binding.saveButton.setEnabled(true);
         binding.deletButton.setEnabled(true);
     }
 
-    public void save(View view){//save butonunun onclick methodu
+    public void save(View view){
 
         Place place=new Place(binding.placeNameText.getText().toString(),selectedLatitude,selectedLongitude);
 
-        //Threading -> MainThread ya da UI Thread denen şeyler var. Ana thread: Ana işlemlerin yapıldığı yer. Kullanıcı arayüzüyle ilgili işlemleri yapıyoruz burada ve eğer burada yüklü işlemler yaparsak eğer kullanıcı arayüzünü bloklayabilir kullanıcının uygulamasını çökertebiliriz. O yüzden tavsiye edilmez. 2tane Thread var. 1) Default (Cpu intentisve)   2)IO (Network, database). Bizden burada istenmeyen şey cpu intensive default olanda işlem yapmamız ama şimdi biz io yani networkte işlem yapıcaz. Network demek te internetten bir veri istemek mesela.
-
-        //placeDao.insert(place).subscribeOn(Schedulers.io()).subscribe();//burada thread'i defaulttan alıp io'a verdik ve uygulamanın kullanıcı arayüzünü çökertmesini engelledik.
-
-        //disposable (kullan at / tek kullanımlık)
-
         compositeDisposable.add(placeDao.insert(place)
-                .subscribeOn(Schedulers.io())//placeDao'yu io thread'de yani networkte yap, kullanıcının arayüzünü çökertme
-                .observeOn(AndroidSchedulers.mainThread()) //io threadde yap ama mainthread de göster, orada gözlemlicem
-                .subscribe(MapsActivity.this::handleResponse));//mapsActivity.this'e subscribe olacağım demek. arkaplanda işlemi yapıcam ama sonucu mainthreadde gözlemlicem demek bu
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread()) 
+                .subscribe(MapsActivity.this::handleResponse));
 
-        //burada aslında 6 satır önce placeDao.insert diye başladığımız kodda yaptığımızın aynısına yakınını yaptık ama o kullandıktan sonra sil ki yer kaplamasın olayı için buraya yazdık. Dispoisble'lar tek kullanımlıktı biliyorsun
 }
 
-    private void handleResponse(){//subscribe işlemi bittikten sonra bir fonksiyon çalıştırmak istiyorsak, örneğin mainActivty'e geri dönmek istiyorum. Buradan yapcaz sonra hemen 2 satır yukarıdaki subscribe() içerisine bu fonksiyonu parantezsiz bir şekilde vericez
+    private void handleResponse(){
         Intent intent=new Intent(MapsActivity.this,MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//flag ekleyip herşeyi temizledik
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
 
-    public void delete(View view){//delete butonunun onclick metodu
+    public void delete(View view){
 
             compositeDisposable.add(placeDao.delete(selectedPlace)
                 .subscribeOn(Schedulers.io())
@@ -277,8 +254,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     @Override
-    protected void onDestroy() {//bunu yazarsam daha önce yazdığım tüm kollar çöpe atılıyor ve hafızada yer tutmuyor. Bu flowable, compotable, singular falan bunlar desposable olarak 1 defa kullanıldıktan sonra çöpe atılmak suretiyle yer kaplamaması sağlanıyor.
+    protected void onDestroy() {
         super.onDestroy();
-        compositeDisposable.clear();//daha önce yaptığım veriler buradan çöpe atılıyor ve hafızamda yer tutmuyor. RxJava'nın kalıntıları atılıyor(flowable, compotable, singular, observable)
+        compositeDisposable.clear();
     }
 }
