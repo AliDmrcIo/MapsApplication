cd0bdd9044c4e9d8f3258200d4e69cce18d262f9
AliBaba61 <110434358+AliBaba61@users.noreply.github.com>
2023-05-17 16:02:13 +0300

    Update PlaceDatabase.java

--- app/src/main/java/com/ali/javamaps/roomDataBase/PlaceDatabase.java
+++ app/src/main/java/com/ali/javamaps/roomDataBase/PlaceDatabase.java
@@ -5,10 +5,9 @@ import androidx.room.RoomDatabase;
 
 import com.ali.javamaps.model.Place;
 
-//Burası database'in teknik kısmı. Burada database'i inşa ettik, o böyle somut ama herşeyin aslı olan yapıyı DATABASE'i burada inşa ettik
 
-@Database(entities = {Place.class},version = 1)//bu versiyonun yazılma sebebi, entitylerde bir değişiklik yapılacağı zaman versiyonu artırmak zorunda kalabiliriz.
-public abstract class PlaceDatabase extends RoomDatabase {//burası RoomDatabase'den extends olan bir abstract olmalı ve burası database'in kendisi. Direkt database'i yarattığmız kısım. Yaratılma kısmı şu şekilde: @Database yazcaz. sonra önceden oluşturdudğumuz entity'i vericez. Sonar versiyonu vericez. Sonra Abstract bir class altında abstract bir method oluşturup yine önceden oluşturduğumuz Dao'yu çağırıcaz.
+@Database(entities = {Place.class},version = 1).
+public abstract class PlaceDatabase extends RoomDatabase {
 
     public abstract PlaceDao placeDao();
 
